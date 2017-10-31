a0d71be7c223956a468248f33cd1953d7d99fc5a
Peter H <one4u2@gmail.com>
2017-10-31 18:55:55 +0000

    testing branch name

--- handler.js
+++ handler.js
@@ -2,47 +2,76 @@
 
 'use strict';
 
+const DynamoDbService = require("./services/dynamoDb_service")
+console.log(DynamoDbService)
 const faker = require("faker")
 
 module.exports.fetch = (event, context, callback) => {
   
-  somedata().then((generatedData) => {
+  const TABLE_NAME = 'blo';
+  const NUM_OF_ITEM = 100;
+  let dynamoDbDataService = new DynamoDbService(TABLE_NAME, NUM_OF_ITEM);
+  
+  
+  dynamoDbDataService.getAll().then((results) => {
      const response = {
-     statusCode: 200,
-     body: JSON.stringify({generatedData}),
-   };
+         statusCode: 200,
+          body: JSON.stringify({results})
+  };  
+    
+    callback(null, response);
+    
+  })
+  .catch((err) => {
+    callback(err)
+  })
+ 
+  
+  
+}
+  
+  
+//   somedata().then((generatedData) => {
+//     const response = {
+//     statusCode: 200,
+//     body: JSON.stringify({generatedData}),
+//   };
    
-   return response;
-  }).then(response => callback(null, response))
+//   return response;
+//   }).then(response => callback(null, response))
+//     .catch((err) => {
+//       if (err)
+//       console.log(err.message)
+//     });
   
   
  
   
   
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+// };
 
 
-const somedata = () => {
-  return new Promise((resolve, reject) => {
+// const somedata = () => {
+//   return new Promise((resolve, reject) => {
     
-    let GeneratedfakerData = [];
+//     let GeneratedfakerData = [];
     
-    for (let i = 0 ; i < 10; i ++) {
-      var randomName = faker.name.findName(); // Rowan Nikolaus
-      var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-      var randomCard = faker.helpers.createCard()
+//     for (let i = 0 ; i < 10; i ++) {
+//       var randomName = faker.name.findName(); // Rowan Nikolaus
+//       var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+//       var randomCard = faker.helpers.createCard()
       
-      GeneratedfakerData.push({randomName, randomEmail, randomCard})
+//       GeneratedfakerData.push({randomName, randomEmail, randomCard})
       
-    }
+//     }
     
-    resolve(GeneratedfakerData)
-  })
+//     resolve(GeneratedfakerData)
+//   })
   
-}
+// }
 
 
 
